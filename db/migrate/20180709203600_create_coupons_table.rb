f04e67e459310be62ec72d7cd8fd5dec4772a14d
Matt Farmer <mrfarmer777@gmail.com>
2018-07-09 20:36:37 +0000

    automatically backed up by learn

--- db/migrate/20180709203600_create_coupons_table.rb
+++ db/migrate/20180709203600_create_coupons_table.rb
@@ -1,6 +1,6 @@
 class CreateCouponsTable < ActiveRecord::Migration
   def change
-    create_table :coupons_table do |t|
+    create_table :coupons do |t|
       t.string :coupon_code
       t.string :store
     end
