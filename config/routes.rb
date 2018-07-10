d347446e375a18b5c1d79bc72d5c454493732b5a
Matt Farmer <mrfarmer777@gmail.com>
2018-07-10 02:02:15 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :coupons, only: :index
+  resources :coupons, only: [:index, :create]
 
   # Example resource route with options:
   #   resources :products do
