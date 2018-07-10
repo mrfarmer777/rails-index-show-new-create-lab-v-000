2b94a2983d1d9d45eb193ba2bfa93b4fc06e625b
Matt Farmer <mrfarmer777@gmail.com>
2018-07-10 02:04:03 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -8,4 +8,5 @@ class CouponsController < ApplicationController
     @coupon=Coupon.create(coupon_code:params[:coupon_code], store:params[:store])
     @coupon.save
     redirect_to coupon_path(@coupon)
+  end
 end
